03f473b01c4e19ad80eeb8462d8dd5857a13cec5
user147796 <user147796@eu503.local>
2018-10-23 13:05:01 -0700

    first commit

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -7,14 +7,15 @@
 //
 
 import UIKit
-
+import MKDataDetector
+import CoreLocation
 class ViewController: UIViewController {
+
+    let dataDetectorService: MKDataDetectorService = MKDataDetectorService()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
     }
 
-
 }
-
