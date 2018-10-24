9b2dd17233667fc4041f0ad7ba62958c859081e4
Mahmod Bassuni <mahmoudbasuni90@gmail.com>
2018-10-24 02:12:06 +0200

    starting in draw annotations in map

--- test/MessageModel.swift
+++ test/MessageModel.swift
@@ -7,12 +7,28 @@
 //
 
 import Foundation
-class MessageModel
+import MapKit
+
+class MessageModel :NSObject , MKAnnotation
 {
-var messageid : Int?
-var message: String?
-var sentiment : String?
-var PlaceName: String?
-var Lat: Double = 0.0
-var lon: Double = 0.0
+    var messageid : Int?
+    var message: String?
+    var sentiment : String?
+    var PlaceName: String?
+    
+     var Lat: CLLocationDegrees = 0
+     var lon: CLLocationDegrees = 0
+    
+    // This property must be key-value observable, which the `@objc dynamic` attributes provide.
+    @objc dynamic var coordinate: CLLocationCoordinate2D {
+        get {
+            return CLLocationCoordinate2D(latitude: Lat, longitude: lon)
+        }
+        set {
+            // For most uses, `coordinate` can be a standard property declaration without the customized getter and setter shown here.
+            // The custom getter and setter are needed in this case because of how it loads data from the `Decodable` protocol.
+            Lat = newValue.latitude
+            lon = newValue.longitude
+        }
+    }
 }
